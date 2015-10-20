071f69f9727696ed5baaf3be28ae4b6ce1b43252
George Bejan <george.bejan@catalysts.cc>
2015-10-20 21:47:45 +0300

    new version

---
+++ javascripts/index.js
@@ -0,0 +1,36 @@
+$( document ).ready(function()  {
+  $("#video-controls").hide();
+  $("#paused").hide();
+
+  $('#get_featured').on('click', function(e) {
+    e.preventDefault();
+    var target= $('#contact')
+    $('html, body').stop().animate({
+       scrollTop: target.offset().top
+    }, 1000);
+  });
+
+  $('#play').on('click', function(e) {
+      var target = $("#header_overlay");
+      $("#header_overlay").hide();
+      $("#video-controls").show();
+      $("#paused").hide();
+  });
+
+  $('#stop').on('click', function(e) {
+      var target = $("#header_overlay");
+      $("#play").css('background-image', 'url(images/play_button.png)');
+      $("#header_overlay").show();
+      $("#video-controls").hide();
+      $("#paused").hide();
+  });
+
+  $('#pause').on('click', function(e) {
+      var target = $("#header_overlay");
+      $("#play").css('background-image', 'url(images/pause_button.png)');
+      $("#paused").show();
+      $("#header_overlay").show();    
+      $("#video-controls").hide();
+  });
+})
+;
