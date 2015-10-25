2ed7ba3ef51ae2c0b3080a5f5eba45e66cff4223
George Bejan <george.bejan@catalysts.cc>
2015-10-25 17:55:48 +0200

    dsadas

--- javascripts/all.js
+++ javascripts/all.js
@@ -10350,36 +10350,39 @@ $( document ).ready(function()  {
   $("#paused").hide();
   $('#confirm_send').hide();
 
-  $('#get_featured').on('click', function(e) {
-    e.preventDefault();
-    var target= $('#contact')
-    $('html, body').stop().animate({
-       scrollTop: target.offset().top
-    }, 1000);
-  });
-
-  $('#play').on('click', function(e) {
-      var target = $("#header_overlay");
-      $("#header_overlay").hide();
-      $("#video-controls").show();
-      $("#paused").hide();
-  });
-
-  $('#stop').on('click', function(e) {
-      var target = $("#header_overlay");
-      $("#play").css('background-image', 'url(images/play_button.png)');
-      $("#header_overlay").show();
-      $("#video-controls").hide();
-      $("#paused").hide();
-  });
-
-  $('#pause').on('click', function(e) {
-      var target = $("#header_overlay");
-      $("#play").css('background-image', 'url(images/play_button.png)');
-      $("#paused").show();
-      $("#header_overlay").show();    
-      $("#video-controls").hide();
-  });
+  var mq = window.matchMedia( "(min-device-width: 850px)" );
+  if (mq.matches) {
+    $('#get_featured').on('click', function(e) {
+      e.preventDefault();
+      var target= $('#contact')
+      $('html, body').stop().animate({
+         scrollTop: target.offset().top
+      }, 1000);
+    });
+    
+    $('#play').on('click', function(e) {
+        var target = $("#header_overlay");
+        $("#header_overlay").hide();
+        $("#video-controls").show();
+        $("#paused").hide();
+    });
+  
+    $('#stop').on('click', function(e) {
+        var target = $("#header_overlay");
+        $("#play").css('background-image', 'url(images/play_button.png)');
+        $("#header_overlay").show();
+        $("#video-controls").hide();
+        $("#paused").hide();
+    });
+  
+    $('#pause').on('click', function(e) {
+        var target = $("#header_overlay");
+        $("#play").css('background-image', 'url(images/play_button.png)');
+        $("#paused").show();
+        $("#header_overlay").show();    
+        $("#video-controls").hide();
+    });
+  }
 
   $('#send_message').submit(function(event) {
       event.preventDefault();
